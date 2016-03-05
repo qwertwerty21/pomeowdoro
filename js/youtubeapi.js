965ce893a70a023a191390a29a5dabc1387eb502
qwertwerty21 <sunnyw1212@gmail.com>
2016-03-05 14:32:19 +0900

    initial commit

---
+++ js/youtubeapi.js
@@ -0,0 +1,28 @@
+var tag = document.createElement( "script" );
+tag.src = "http://www.youtube.com/iframe_api";
+
+var $firstScript = $( "script" ).first();
+$firstScript.parent().append( tag );
+
+var player;
+var catIframeYT;
+
+window.onYouTubeIframeAPIReady = function(){
+	
+	player = new YT.Player( "catShowerDiv", {
+		playerVars:{
+			listType: "playlist",
+			list: "PLYnGL4xCCL3J6UKqjhJAIMhZCgOVL1tjW"
+		},
+		events: {
+			"onReady": onPlayerReady
+		}
+	
+	});
+	
+}
+
+function onPlayerReady( event ){
+	catIframeYT = event.target;
+	catIframeYT.pauseVideo();
+}
