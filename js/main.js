51666008a7c09bb8c9ce77ec26fd1edbf42f8945
qwertwerty21 <sunnyw1212@gmail.com>
2016-03-12 16:21:40 +0900

    feature detection for Notifications

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
 
 $(document).ready(function(){
 
-	if (Notification.permission !== "granted"){
+	if( typeof Notification === 'function' ){
+		if( Notification.permission !== "granted" ){
     	Notification.requestPermission();
+		}
 	}
+
+	
 	
 	var myPomoTimer = new Timer( ( 60000 * 25 ) , ( 60000 * 4) );
 	myPomoTimer.init();
